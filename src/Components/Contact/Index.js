87723346aa07fc3d861f1eb099c4e9077612896d
Nicogz19 <niicogonzalez98@gmail.com>
2022-09-12 15:27:21 -0300

    I add logic to the contact form

--- src/Components/Contact/Index.js
+++ src/Components/Contact/Index.js
@@ -1,16 +1,19 @@
-import { Box, Button, Grid, TextField, Typography } from '@mui/material'
+import { Box, Grid, TextField, Typography } from '@mui/material'
 import React, {useState} from 'react'
 import './style.css'
 import emailJs from '@emailjs/browser'
 import contactImg from '../../img/Mention-amico.png'
 import { styled } from '@mui/material/styles';
+import CheckIcon from '@mui/icons-material/Check';
+import LoadingButton from '@mui/lab/LoadingButton';
+import SendIcon from '@mui/icons-material/Send';
 
 const CustomTextField = styled(TextField)({
     '& label.Mui-focused': {
-      color: 'white',
+        color: 'white',
     },
     '& .MuiInput-underline:after': {
-      borderBottomColor: 'white',
+        borderBottomColor: 'white',
     },
     '& .MuiInputBase-root':{
         '& .MuiInputBase-input': {
@@ -18,35 +21,74 @@ const CustomTextField = styled(TextField)({
         },
     },
     '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: 'white',
-      },
-      '&:hover fieldset': {
-        borderColor: 'white',
-        border: '2px solid white'
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: 'white',
-      },
+        '& fieldset': {
+            borderColor: 'white',
+        },
+        '&:hover fieldset': {
+            borderColor: 'white',
+            border: '2px solid white'
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: 'white',
+        },
     },
+    '& .MuiFormHelperText-root.Mui-error': {
+        color: '#720000',
+        fontWeight: 'bolder'
+    },
+});
+
+const CustomButton = styled(LoadingButton)({
+    '& .MuiLoadingButton-loadingIndicator': {
+        right: 200,
+        color: 'white'
+    },
+    '& .MuiButtonBase-root .Mui-disabled':{
+        color: 'white!important',
+        margin: 100
+    }
 });
 
 const Contact = () => {
 
-    const [error, setError] = useState(false)
+    const [send, sendEmail] = useState(false)
+    const [loading, setLoading] = useState(false);
+    const [successfully, sendSuccessfully] = useState(false)
+    const [value, setValue] = useState({
+        user_name: '', 
+        user_email: '', 
+        user_message: ''
+    })
 
-    const sendEmail = (e) => {
-        console.log(e.target.value, 'e.target.value')
+    const handleChange = (e) => {
+        setValue({
+            ...value,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const sendEmailForm = (e) => {
+        sendEmail(true)
+        setLoading(true)
         e.preventDefault();
 
-        setError(e.target.value ? false : true)
-    
-        emailJs.sendForm('service_9hd51uc', 'template_mf0argf', e.target, 'EaLQCAnDBySBm3R-h')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
+        if(value.user_name && value.user_email && value.user_message){
+            emailJs.sendForm('service_rb3kypi', 'template_mf0argf', e.target, 'EaLQCAnDBySBm3R-h')
+            .then((result) => {
+                if(result.text === 'OK'){
+                    sendSuccessfully(true)
+                    setLoading(false)
+                    sendEmail(false)
+                    setValue({user_name: '', user_email: '', user_message: ''})
+                }
+            }, (error) => {
+                setLoading(false)
+                sendSuccessfully(false)
+            });
+        } else{
+            setLoading(false)
+            sendSuccessfully(false)
+        }
     };
 
     return (
@@ -57,38 +99,53 @@ const Contact = () => {
                     <img className='contactImg' src={contactImg} alt='contactImg' />
                 </Grid>
                 <Grid className='formContactContainer' item xs={11} sm={11} md={6}>
-                    <Box className='formContact' component='form' onSubmit={sendEmail}>
+                    <Box className='formContact' component='form' onSubmit={sendEmailForm}>
                         <CustomTextField 
                             label='Name' 
                             name='user_name' 
                             fullWidth 
-                            error={error} 
-                            helperText={error && 'complete the Name field'} 
+                            error={!value.user_name && send} 
+                            helperText={!value.user_name && send && 'complete the Name field'} 
                             variant='outlined' 
                             className='input'
                             color='secondary'
+                            onChange={(e) => handleChange(e)}
+                            value={value.user_name}
                         />
                         <CustomTextField 
                             label='Email' 
                             name='user_email' 
                             fullWidth 
-                            error={error} 
-                            helperText={error && 'complete the Email field'} 
+                            error={!value.user_email && send} 
+                            helperText={!value.user_email && send && 'complete the Email field'} 
                             variant='outlined' 
                             className='input'
+                            onChange={(e) => handleChange(e)}
+                            value={value.user_email}
                         />
                         <CustomTextField 
                             label='Message' 
                             name='user_message' 
                             fullWidth 
-                            error={error} 
-                            helperText={error && 'complete the Message field'} 
+                            error={!value.user_message && send} 
+                            helperText={!value.user_message && send && 'complete the Message field'} 
                             multiline 
                             maxRows={4} 
                             variant='outlined'
                             className='input'
+                            onChange={(e) => handleChange(e)}
+                            value={value.user_message}
                         />
-                        <Button type='submit' variant='contained'>Send Email</Button>
+                        <CustomButton
+                            endIcon={<SendIcon />}
+                            loading={loading}
+                            loadingPosition="end"
+                            variant="contained"
+                            type='submit'
+                        >
+                            Send Email
+                        </CustomButton>
+                        {successfully && <Box className='successfullyMessage'><Typography>Email sent successfully </Typography><CheckIcon /></Box>}
                     </Box>
                 </Grid>
             </Grid>
